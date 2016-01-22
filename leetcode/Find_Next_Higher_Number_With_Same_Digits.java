2e2c714900c5b9f84266d3deb06ea893bebde5f1
Debosmit Ray <dray92@uw.edu>
2016-01-21 22:48:42 -0800

    fix edge case

--- leetcode/Find_Next_Higher_Number_With_Same_Digits.java
+++ leetcode/Find_Next_Higher_Number_With_Same_Digits.java
@@ -14,56 +14,57 @@ public class Find_Next_Higher_Number_With_Same_Digits {
 		int pivot;
 		
 		// get the digit that has to be switched
-		for(pivot = val.length() - 2 ; pivot >=0 ; pivot--) 
-			if(val.charAt(pivot) < val.charAt(pivot+1))
-				break;
-		
-		// if everything to the right of this pivot
-		// is sorted, the smallest of that subset of
-		// digits will be at pivot+1; this digit will
-		// not necessarily be greater than the digit at pivot; 
-		// swapping pivot with a number in the sorted set
-		// that is greater than the pivot value will yield
-		// the required number
-		
-		// need to sort indices [pivot+1, val.length()-1]
-		// single pass radix sort would work since we have
-		// only digits ranging from 0 to 9.
-		
-		// get digits for the remainder of the list
-		int numDigits = val.length() - (pivot + 1);
-		int[] digits = new int[numDigits];
-		for(int i = 0 ; i < digits.length ; i++)
-			digits[i] = val.charAt(i + (pivot + 1)) - '0';
-		
-		// sort the digits
-		// note: sorting can also be replaced with reversing
-		// order of digits since they must be a0 >= a1 >= a3 ...
-		RadixSort Sort = new RadixSort();
-		Sort.sort(digits);
-		
-		// swap index pivot with appropriate
-		// value in digits that is greater than current
-		// value of pivot;
-		// after this process, array must remain sorted
-		// since oldPivotValue is being put at the index of value 
-		// that is greater than it; it is also ensured that the 
-		// values at all indices before this are less than 
-		// or equal to the oldPivotValue
-		int oldPivotVal = val.charAt(pivot) - '0';
-		for(int i = 0 ; i < digits.length ; i++) {
-			if(digits[i] > oldPivotVal) {
-				val.setCharAt(pivot, (char)(digits[i] + '0'));
-				digits[i] = oldPivotVal;
+		for(pivot = val.length() - 2 ; pivot >=0 ; pivot--) {
+			if(val.charAt(pivot) < val.charAt(pivot+1)) {
+				// if everything to the right of this pivot
+				// is sorted, the smallest of that subset of
+				// digits will be at pivot+1; this digit will
+				// not necessarily be greater than the digit at pivot; 
+				// swapping pivot with a number in the sorted set
+				// that is greater than the pivot value will yield
+				// the required number
+				
+				// need to sort indices [pivot+1, val.length()-1]
+				// single pass radix sort would work since we have
+				// only digits ranging from 0 to 9.
+				
+				// get digits for the remainder of the list
+				int numDigits = val.length() - (pivot + 1);
+				int[] digits = new int[numDigits];
+				for(int i = 0 ; i < digits.length ; i++)
+					digits[i] = val.charAt(i + (pivot + 1)) - '0';
+				
+				// sort the digits
+				// note: sorting can also be replaced with reversing
+				// order of digits since they must be a0 >= a1 >= a3 ...
+				RadixSort Sort = new RadixSort();
+				Sort.sort(digits);
+				
+				// swap index pivot with appropriate
+				// value in digits that is greater than current
+				// value of pivot;
+				// after this process, array must remain sorted
+				// since oldPivotValue is being put at the index of value 
+				// that is greater than it; it is also ensured that the 
+				// values at all indices before this are less than 
+				// or equal to the oldPivotValue
+				int oldPivotVal = val.charAt(pivot) - '0';
+				for(int i = 0 ; i < digits.length ; i++) {
+					if(digits[i] > oldPivotVal) {
+						val.setCharAt(pivot, (char)(digits[i] + '0'));
+						digits[i] = oldPivotVal;
+						break;
+					}
+				}
+				
+				// replace indices [pivot+1, val.length()-1] with
+				// values from sorted array
+				for(int i = 0 ; i < digits.length ; i++)
+					val.setCharAt(i + (pivot + 1), (char)(digits[i] + '0'));
 				break;
 			}
 		}
-		
-		// replace indices [pivot+1, val.length()-1] with
-		// values from sorted array
-		for(int i = 0 ; i < digits.length ; i++)
-			val.setCharAt(i + (pivot + 1), (char)(digits[i] + '0'));
-		
+		// if number is as big as it can get, val stays unchanged
 		return Integer.parseInt(val.toString());
 	}
 	
@@ -80,6 +81,10 @@ public class Find_Next_Higher_Number_With_Same_Digits {
 		System.out.println("Next_Higher_Number_With_Same_Digits: " + Next.getNextHighest(num));
 		
 		num = 136442;
+		System.out.println("\n\nNum: " + num);
+		System.out.println("Next_Higher_Number_With_Same_Digits: " + Next.getNextHighest(num));
+		
+		num = 776442;
 		System.out.println("\n\nNum: " + num);
 		System.out.println("Next_Higher_Number_With_Same_Digits: " + Next.getNextHighest(num));
 		
