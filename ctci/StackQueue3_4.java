d1e96dca8747ca91be35ebf47411ae91001e378f
Debosmit Ray <dray92@uw.edu>
2016-01-25 13:14:24 -0800

    no message

--- ctci/StackQueue3_4.java
+++ ctci/StackQueue3_4.java
@@ -17,7 +17,20 @@ public class StackQueue3_4 {
 		towers = new ArrayList<Stack<Integer>>();
 	}
 	
-	public void initializeTowers() {
+	public void moveTop(int fromTowerIndex, int toTowerIndex) throws Exception {
+		Stack<Integer> toTower = towers.get(toTowerIndex);
+		int top = towers.get(fromTowerIndex).pop();
+		
+		// if element present is of lesser value
+		if(!toTower.isEmpty() && toTower.peek() <= top)
+			throw new Exception("Cannot move top element from "
+					+ "tower" + fromTowerIndex + " to tower " + toTowerIndex);
+		
+		toTower.push(top);
+		
+	}
+	
+	public void initializeTowers(int max) {
 		for(int i = 0; i < 3 ; i++) {
 			Stack<Integer> tower = new Stack<Integer>();
 			towers.add(tower);
@@ -37,15 +50,28 @@ public class StackQueue3_4 {
 			System.err.println("Initialization failed");
 			System.exit(0);
 		}
-		firstTower.add(3);
-		firstTower.add(2);
-		firstTower.add(1);
+		while(max > 0)
+			firstTower.add(max--);
 	}
 	
-	public static void main(String[] args) {
+	public void moveDisks(int numDisks, int destTowerIndex, int bufTowerIndex, 
+			int srcTowerIndex) throws Exception {
+		if(numDisks > 0) {
+			moveDisks(numDisks - 1, bufTowerIndex, destTowerIndex,srcTowerIndex );
+			moveTop(srcTowerIndex, destTowerIndex);
+			moveDisks(numDisks - 1, destTowerIndex, srcTowerIndex,bufTowerIndex );
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
 		StackQueue3_4 hanoi = new StackQueue3_4();
-		hanoi.initializeTowers();
+		int numDisks = 4;
+		hanoi.initializeTowers(numDisks);
 		System.out.println("Towers initialized");
+		System.out.println("Towers: " + hanoi.towers);
+		int destTower = 2, bufTower = 1, srcTower = 0;
+		hanoi.moveDisks(numDisks, destTower, bufTower, srcTower);
+		System.out.println("Towers: " + hanoi.towers);
 	}
-	
 }
