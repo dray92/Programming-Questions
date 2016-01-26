bcbd06bcd0a3c18fb10db1e36f4251064c920fb4
Debosmit Ray <dray92@uw.edu>
2016-01-25 17:45:41 -0800

    no message

---
+++ ctci/StackQueue3_7.java
@@ -0,0 +1,15 @@
+package ctci;
+
+/**
+ * An animal shelter holds only dogs or cats.
+ * The follow a strict 'first in first out' 
+ * policy. You either pick the oldest animal
+ * or the oldest cat/dog if you have a preference.
+ * Support enqueue, dequeue, dequeueDog, dequeueCat.
+ * Implement such a data structure.
+ * @author Debosmit
+ *
+ */
+public class StackQueue3_7 {
+
+}
