75f16cad226b65cc96a5e0823e09f4473c4b9c28
Debosmit Ray <dray92@uw.edu>
2016-03-09 23:47:20 -0800

    init

---
+++ leetcode/Number_of_Triangles.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * Given an unsorted array of positive integers. Find the 
+ * number of triangles that can be formed with three 
+ * different array elements as three sides of triangles. 
+ * For a triangle to be possible from 3 values, the sum of any 
+ * two values (or sides) must be greater than the third value (or third side).
+ * @author Debosmit
+ *
+ */
+public class Number_of_Triangles {
+
+}
