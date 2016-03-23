432ef04fc30bd9bdb2c5a787e4ce4c35122e859b
Debosmit Ray <dray92@uw.edu>
2016-03-23 13:16:31 -0700

    done

--- projectEuler/Problem3.java
+++ projectEuler/Problem3.java
@@ -58,13 +58,15 @@ public class Problem3 {
 	}
 	
 	public static BigInteger sqrt2(BigInteger n) {
-		  BigInteger a = BigInteger.ONE;
-		  BigInteger b = new BigInteger(n.shiftRight(5).add(new BigInteger("8")).toString());
-		  while(b.compareTo(a) >= 0) {
-		    BigInteger mid = new BigInteger(a.add(b).shiftRight(1).toString());
-		    if(mid.multiply(mid).compareTo(n) > 0) b = mid.subtract(BigInteger.ONE);
-		    else a = mid.add(BigInteger.ONE);
-		  }
-		  return a.subtract(BigInteger.ONE);
-		}
+	    BigInteger a = BigInteger.ONE;
+	    BigInteger b = n.shiftRight(1).add(new BigInteger("2")); // (n >> 1) + 2 (ensure 0 doesn't show up)
+	    while (b.compareTo(a) >= 0) {
+	        BigInteger mid = a.add(b).shiftRight(1); // (a+b) >> 1
+	        if (mid.multiply(mid).compareTo(n) > 0)
+	            b = mid.subtract(BigInteger.ONE);
+	        else
+	            a = mid.add(BigInteger.ONE);
+	    }
+	    return a.subtract(BigInteger.ONE);
+	}
 }
