f6c59e3312c9b47f2c3c72d9fcf539ad8a73af99
Debosmit Ray <dray92@uw.edu>
2016-03-26 00:55:13 -0700

    changed data structure

--- leetcode/Permutations_of_String.java
+++ leetcode/Permutations_of_String.java
@@ -1,23 +1,25 @@
 package leetcode;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
 
 public class Permutations_of_String {
 
-	public List<String> getPermutations(String st) {
+	public Set<String> getPermutations(String st) {
 		if(st.length() == 0)
-			return new ArrayList<String>();
+			return new HashSet<String>();
 		
 		if(st.length() == 1) {
-			List<String> list = new ArrayList<String>();
+			Set<String> list = new HashSet<String>();
 			list.add(st);
 			return list;
 		}
 		
-		List<String> permutations = getPermutations(st.substring(1));
+		Set<String> permutations = getPermutations(st.substring(1));
 		char ch = st.charAt(0);
-		List<String> result = new ArrayList<String>();
+		Set<String> result = new HashSet<String>();
 		for(String permutation: permutations) {
 			StringBuilder permutationSB = new StringBuilder(permutation);
 			for(int i = 0 ; i <= permutationSB.length() ; i++) {
@@ -33,7 +35,7 @@ public class Permutations_of_String {
 	public static void main(String[] args) {
 		Permutations_of_String permutationGenerator = new Permutations_of_String();
 		String st = "aa";
-		List<String> words = permutationGenerator.getPermutations(st);
+		Set<String> words = permutationGenerator.getPermutations(st);
 		System.out.println("Word: " + st);
 		System.out.print("Permutations: ");
 		for(String word: words)
