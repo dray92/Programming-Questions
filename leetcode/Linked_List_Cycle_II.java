53395ff87e570ea200e4f276be3412349ddfc4a0
Debosmit Ray <dray92@uw.edu>
2016-04-19 11:27:37 -0700

    done

---
+++ leetcode/Linked_List_Cycle_II.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class Linked_List_Cycle_II {
+
+	public boolean hasCycle(ListNode head) {
+        if(head == null)
+            return false;
+        
+        ListNode slow = head, fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(fast == slow || fast.next == slow)
+                return true;
+        }
+        return false;
+    }
+	
+	class ListNode {
+		 int val;
+		 ListNode next;
+		 ListNode(int x) {
+		 	val = x;
+		 	next = null;
+		 }
+	}
+}
